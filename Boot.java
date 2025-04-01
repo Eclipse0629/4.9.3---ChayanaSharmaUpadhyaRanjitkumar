4a988b7ee449ee6b1ef99539d965e8fbe5e1be77
Ujjwal Ranjit <159827223+Eclipse0629@users.noreply.github.com>
2025-04-01 15:04:29 -0700

    Update Boot.java

--- Boot.java
+++ Boot.java
@@ -3,5 +3,15 @@
  */
 public class Boot extends LakeObject
 {
+  @Override
+  public String say()
+  {
+    return "You caught an old boot!";
+  }
 
+  @Override
+  public boolean wasCaught(Hook h)
+  {
+    return true;
+  }
 }
